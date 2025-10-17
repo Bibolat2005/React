825a5e160e00242199a707e595c871ab45b60eb8
Bibolat Kaldybay <bibolat.kaldybay@netcracker.com>
2025-10-17 17:09:16 +0500

    commit

---
+++ lab4/src/Movies/MoviesList.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import MovieCard from "../Card/MovieCard";
+import "./MoviesList.css";
+
+function MoviesList() {
+  const [movies, setMovies] = useState([]);
+  const [filterQuery, setFilterQuery] = useState("");
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://imdb.iamidiotareyoutoo.com/search?q=Horror"
+    );
+    const data = await response.json();
+    setMovies(data.description);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie["#TITLE"]?.toLowerCase().includes(filterQuery.toLowerCase())
+  );
+
+  const clearSearch = () => setFilterQuery("");
+
+  return (
+    <div className="movies-list-container">
+      {/* Поле поиска и кнопка очистки */}
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          value={filterQuery}
+          onChange={(e) => setFilterQuery(e.target.value)}
+        />
+        <button className="clear-btn" onClick={clearSearch}>
+          Clear
+        </button>
+      </div>
+
+      <button className="load-button" onClick={fetchMovies}>
+        Click to see genres
+      </button>
+
+      <ul className="movies-list">
+        {filteredMovies.map((movie, index) => (
+          <li className="movies-item" key={index}>
+            <MovieCard movie={movie} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default MoviesList;
